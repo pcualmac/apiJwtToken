092f0095450fb46cbef5a19b4f32034593ee3b97
pcualmac <jfpum.cal@gmail:.com>
2025-02-25 12:55:58 +0000

    chenge and use BCryptPasswordEncoder

--- apiJwtToken/src/main/java/com/example/apiJwtToken/config/SecurityConfig.java
+++ apiJwtToken/src/main/java/com/example/apiJwtToken/config/SecurityConfig.java
@@ -2,24 +2,13 @@ package com.example.apiJwtToken.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/register").permitAll() // Allow unauthenticated access
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
-        return http.build();
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
