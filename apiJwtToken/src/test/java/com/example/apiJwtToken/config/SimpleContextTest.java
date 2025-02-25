22de4e90d2c79978a00678024749afd64bd818b9
pcualmac <jfpum.cal@gmail:.com>
2025-02-25 12:57:39 +0000

    clean SimpleContextTest

--- apiJwtToken/src/test/java/com/example/apiJwtToken/config/SimpleContextTest.java
+++ apiJwtToken/src/test/java/com/example/apiJwtToken/config/SimpleContextTest.java
@@ -3,7 +3,7 @@ import com.example.apiJwtToken.ApiJwtTokenApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = ApiJwtTokenApplication.class)
+@SpringBootTest()
 public class SimpleContextTest {
 
     @Test
